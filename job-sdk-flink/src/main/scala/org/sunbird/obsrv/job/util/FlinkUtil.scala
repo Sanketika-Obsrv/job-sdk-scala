b4e52c06c1f5fb54188dac2b8fd204eaf0252f24
Ravi Mula <ravismula@users.noreply.github.com>
2024-09-03 14:46:01 +0530

    read connector id from args

--- job-sdk-flink/src/main/scala/org/sunbird/obsrv/job/util/FlinkUtil.scala
+++ job-sdk-flink/src/main/scala/org/sunbird/obsrv/job/util/FlinkUtil.scala
@@ -2,6 +2,7 @@ package org.sunbird.obsrv.job.util
 
 import com.typesafe.config.Config
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.runtime.state.hashmap.HashMapStateBackend
 import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
@@ -9,8 +10,9 @@ import org.apache.flink.streaming.api.environment.{CheckpointConfig, StreamExecu
 
 object FlinkUtil {
 
-  def getExecutionContext(config: Config): StreamExecutionEnvironment = {
+  def getExecutionContext(args: Array[String], config: Config): StreamExecutionEnvironment = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val connectorId = Option(ParameterTool.fromArgs(args).get("metadata.id")).getOrElse(config.getString("metadata.id"))
     env.getConfig.setUseSnapshotCompression(if (config.hasPath("job.enable.distributed.checkpointing")) config.getBoolean("job.enable.distributed.checkpointing") else false)
     env.enableCheckpointing(config.getInt("task.checkpointing.interval"))
     if (config.hasPath("job.enable.distributed.checkpointing") && config.getBoolean("job.enable.distributed.checkpointing")) {
@@ -20,7 +22,7 @@ object FlinkUtil {
       checkpointConfig.setExternalizedCheckpointCleanup(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
       checkpointConfig.setMinPauseBetweenCheckpoints(config.getInt("task.checkpointing.pause.between.seconds"))
       checkpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-      checkpointConfig.setCheckpointStorage(s"${checkpointingBaseUrl.getOrElse("")}/${config.getString("metadata.id")}")
+      checkpointConfig.setCheckpointStorage(s"${checkpointingBaseUrl.getOrElse("")}/${connectorId}")
     }
     env.setRestartStrategy(RestartStrategies.fixedDelayRestart(config.getInt("task.restart-strategy.attempts"), config.getLong("task.restart-strategy.delay")))
     env
